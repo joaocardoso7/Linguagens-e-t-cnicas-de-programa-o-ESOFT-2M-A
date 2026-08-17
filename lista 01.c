f258a0970bb9dffb58be40abcb2ba94fa415fc5c
joaocardoso7 <joaovitor2008cardoso@gmail.com>
2026-08-17 17:59:11 -0300

    lista 1 atualizada

--- lista 01.c
+++ lista 01.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main(int argc, char *argv[]) {
-	
-	//*Faça um programa que leia dois números inteiros e depois os imprima na ordem inversa em que eles foram lidos
-    
+
+    // Ler dois inteiros e imprimir na ordem inversa
     int primeiro, segundo, aux;
-    
+
     printf("\ninsira o primeiro valor: ");
     scanf("%d", &primeiro);
 
@@ -18,50 +16,47 @@ int main(int argc, char *argv[]) {
     primeiro = segundo;
     segundo = aux;
 
-    printf("\nValores na ordem inversa:\n%d\n%d\n", primeiro, segundo); //ordem inversa
+    printf("\nValores na ordem inversa:\n%d\n%d\n", primeiro, segundo);
 
-    
-    //Faça um programa que leia um valor do tipo double e depois o imprima na forma de notação científica.
-    
-    double numero; 
-    
-    printf("\ninsira o valor do numero para converte-lo em notação cientifica:");
-    scanf("%le", &numero);
-    
-    printf("\nO valor em notacao cientifica e: %e", numero);
-    
-    //Implemente um programa que leia um número n e mostre na tela o seu valor em base binária (bits).
-    
-  int n;
 
-    printf("\nDigite um numero inteiro:");
+    // Ler double e imprimir em notacao cientifica
+    double numero;
+
+    printf("\ninsira o valor do numero para converte-lo em notacao cientifica: ");
+    scanf("%lf", &numero); 
+
+    printf("O valor em notacao cientifica e: %e\n", numero);
+
+
+    // Imprimir numero em binario 
+    int n;
+
+    printf("\nDigite um numero inteiro: ");
     scanf("%d", &n);
 
-    int b64 = (n / 64) % 2; 
-    int b32 = (n / 32) % 2; 
-    int b16 = (n / 16) % 2; 
+    int b64 = (n / 64) % 2;
+    int b32 = (n / 32) % 2;
+    int b16 = (n / 16) % 2;
     int b8  = (n / 8)  % 2;
-    int b4  = (n / 4)  % 2; 
-    int b2  = (n / 2)  % 2; 
-    int b1  = (n / 1)  % 2; 
-    
-   printf("Em binario: %d%d%d%d%d%d%d\n", b64, b32, b16, b8, b4, b2, b1);
-   
-   /*(URI 1009) Faça um programa que leia, o salário fixo e o valor total em vendas de um vendedor.
-Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas, mostre o total a
-receber no final do mês, com duas casas decimais.*/
-   
-   float salario, comissao, totalvendido, totalareceber;
- 
-   
-   printf("digite seu salario:");
-   scanf("%f", &salario);
-   
-   printf("agora digite ")
-   
-  
-   
+    int b4  = (n / 4)  % 2;
+    int b2  = (n / 2)  % 2;
+    int b1  = (n / 1)  % 2;
 
-    
-	return 0;
+    printf("Em binario: %d%d%d%d%d%d%d\n", b64, b32, b16, b8, b4, b2, b1);
+
+
+    // (URI 1009) Salario com comissao de 15%
+    double salario, totalvendido, totalareceber;
+
+    printf("\ndigite seu salario: ");
+    scanf("%lf", &salario);
+
+    printf("digite o total vendido: ");
+    scanf("%lf", &totalvendido);
+
+    totalareceber = salario + (totalvendido * 0.15);
+
+    printf("TOTAL = R$ %.2lf\n", totalareceber);
+
+    return 0;
 }
