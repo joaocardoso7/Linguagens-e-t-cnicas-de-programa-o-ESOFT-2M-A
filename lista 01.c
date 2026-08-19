a4972d9199d6719d89a96c2afbb325af0d785cec
joaocardoso7 <joaovitor2008cardoso@gmail.com>
2026-08-19 10:45:05 -0300

    Refactor and add new exercises in lista 01.c
    Added exercises to calculate age in days, volume of a sphere, and Euclidean distance between two points.

--- lista 01.c
+++ lista 01.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define PI 3.141592653589793
 
 int main(int argc, char *argv[]) {
 
     // Ler dois inteiros e imprimir na ordem inversa
+    printf("\nEXERCICIO 1\n");
     int primeiro, segundo, aux;
 
-    printf("\ninsira o primeiro valor: ");
+    printf("Insira o primeiro valor: ");
     scanf("%d", &primeiro);
 
-    printf("\ninsira o segundo valor: ");
+    printf("Insira o segundo valor: ");
     scanf("%d", &segundo);
 
     aux = primeiro;
@@ -19,19 +23,21 @@ int main(int argc, char *argv[]) {
     printf("\nValores na ordem inversa:\n%d\n%d\n", primeiro, segundo);
 
 
-    // Ler double e imprimir em notacao cientifica
+    // EXERCICIO 2: Ler double e imprimir em notacao cientifica
+    printf("\nEXERCICIO 2\n");
     double numero;
 
-    printf("\ninsira o valor do numero para converte-lo em notacao cientifica: ");
+    printf("Insira o valor para converter em notacao cientifica: ");
     scanf("%lf", &numero); 
 
     printf("O valor em notacao cientifica e: %e\n", numero);
 
 
-    // Imprimir numero em binario 
+    //Imprimir numero em binario (7 bits)
+    printf("\nEXERCICIO 3\n");
     int n;
 
-    printf("\nDigite um numero inteiro: ");
+    printf("Digite um numero inteiro: ");
     scanf("%d", &n);
 
     int b64 = (n / 64) % 2;
@@ -45,46 +51,87 @@ int main(int argc, char *argv[]) {
     printf("Em binario: %d%d%d%d%d%d%d\n", b64, b32, b16, b8, b4, b2, b1);
 
 
-    // (URI 1009) Salario com comissao de 15%
+    //  Salario com comissao de 15% 
+    printf("\nEXERCICIO 4\n");
     double salario, totalvendido, totalareceber;
 
-    printf("\ndigite seu salario: ");
+    printf("Digite seu salario fixo: ");
     scanf("%lf", &salario);
 
-    printf("digite o total vendido: ");
+    printf("Digite o total vendido: ");
     scanf("%lf", &totalvendido);
 
     totalareceber = salario + (totalvendido * 0.15);
 
     printf("TOTAL = R$ %.2lf\n", totalareceber);
 
-    /*Elabore um programa que peça ao usuário para digitar 4 valores. E mostre na tela a soma, a média e o
-produtório desses valores*/
-    
+
+    //  Soma, media e produtorio de 4 valores
+    printf("\nEXERCICIO 5\n");
     float valor1, valor2, valor3, valor4, soma, media, produtorio;
     
+    printf("Digite o valor (1/4): ");
+    scanf("%f", &valor1);
     
-    
-      printf("digite um numero para efetuarmos o calculo(1/4):");
-      scanf("%f", &valor1);
-    
-    printf("digite mais um numero (2/4):");
+    printf("Digite o valor (2/4): ");
     scanf("%f", &valor2);
     
-    printf("mais um (3/4):");
+    printf("Digite o valor (3/4): ");
     scanf("%f", &valor3); 
     
-    printf("ultimo valor(4/4):");
+    printf("Digite o valor (4/4): ");
     scanf("%f", &valor4);
     
-     soma = valor1 + valor2 + valor3 + valor4;
-    media = soma / 4;
+    soma = valor1 + valor2 + valor3 + valor4;
+    media = soma / 4.0f;
     produtorio = valor1 * valor2 * valor3 * valor4;
     
-     printf("\nsoma: %.2f\n", soma);
-    printf("media: %.2f\n", media);
-    printf("produtorio: %.2f\n", produtorio);
-    
-    
+    printf("\nSoma: %.2f\n", soma);
+    printf("Media: %.2f\n", media);
+    printf("Produtorio: %.2f\n", produtorio);
+
+
+    // Converter idade em dias para anos, meses e dias
+    printf("\nEXERCICIO 6\n");
+    int idade_dias, anos, meses, dias, resto;
+
+    printf("Digite sua idade em dias: ");
+    scanf("%d", &idade_dias);
+
+    anos = idade_dias / 365;
+    resto = idade_dias % 365;
+
+    meses = resto / 30;
+    dias = resto % 30;
+
+    printf("%d ano(s), %d mes(es) e %d dia(s)\n", anos, meses, dias);
+
+
+    // Volume da esfera
+    printf("\nEXERCICIO 7\n");
+    double raio, volume;
+
+    printf("Digite o raio da esfera: ");
+    scanf("%lf", &raio);
+
+    volume = (4.0 / 3.0) * PI * pow(raio, 3);
+
+    printf("O volume da esfera e: %.2lf m^3\n", volume);
+
+
+    // Distancia euclidiana entre dois pontos
+    printf("\nEXERCICIO 8\n");
+    float x1, x2, y1, y2, distancia;
+
+    printf("Digite os valores de x1 e y1 (separados por espaco): ");
+    scanf("%f %f", &x1, &y1);
+
+    printf("Digite os valores de x2 e y2 (separados por espaco): ");
+    scanf("%f %f", &x2, &y2);
+
+    distancia = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+
+    printf("A distancia entre os pontos e: %.2f\n", distancia);
+
     return 0;
 }
