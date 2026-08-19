e9a05d64000234e749d82f1ee08048ec91a44ef4
joaocardoso7 <joaovitor2008cardoso@gmail.com>
2026-08-19 11:14:16 -0300

    Add speed conversion from km/h to m/s
    Added a new exercise to convert speed from km/h to m/s.

--- lista2.c
+++ lista2.c
@@ -4,20 +4,35 @@
 
 int main(int argc, char *argv[]) {
 	
-	//Fa�a um programa que calcule o ano de nascimento de uma pessoa a partir de sua idade e do ano atual
+	//Faça um programa que calcule o ano de nascimento de uma pessoa a partir de sua idade e do ano atual
 	printf("exercicio 1\n");
 	int idade, anoatual, anodenascimento;
 	
 	printf("digite sua idade:\n");
 	scanf("%d", &idade);
 	
-	printf("\nagora digite o ano em que estamos:");
+	printf("\nagora digite o ano em que estamos:\n");
 	scanf("%d", &anoatual);
 	
 	anodenascimento = anoatual - idade;
 	
 	printf("seu ano de nascimento e: %d", anodenascimento);
 	
+ /*Leia uma velocidade em km/h (quilômetros por hora) e apresente convertida em m/s (metros por
+segundo). A fórmula de conversão é M = K/36, sendo K a velocidade em km/h e M em m/s*/
+
+printf("exercicio 2");
+    float K, M;
+
+    printf("Digite a velocidade em km/h: ");
+    scanf("%f", &K);
+
+    
+    M = K / 3.6;
+
+    printf("Velocidade em m/s: %.2f\n", M);
+
+
 	
 	return 0;
 }
